1f6c306e301f3fe35f121ce4913275ac8a9b744b
Zohaib Ali <www.zohaib.0335@gmail.com>
2022-12-27 20:47:04 +0500

    add hero section & swiperJS

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
 // Mobile Navigation Menu
 let navVisible = false
+const hamburger = document.querySelector('#hamburger')
 console.log(navVisible)
 const mobileMenu = document.querySelector('#mobile-menu')
 document.querySelector('#nav-trigger').addEventListener('click', ()=> {
   if (!navVisible) {
     mobileMenu.style.left = 0
     navVisible = true
+    hamburger.innerText = 'arrow_back_ios'
     console.log(navVisible)
   } else {
     mobileMenu.style.left = '-200px'
     navVisible = false
+    hamburger.innerText = 'menu'
     console.log(navVisible)
   }
 })
